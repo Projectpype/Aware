04bda7585d5a00ee15523efe35925967c7333c08
Projectpype <43429889+Projectpype@users.noreply.github.com>
2018-09-20 00:12:03 -0500

    Edit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              Making earth a better place.
+              Making earth a better place together.
             </Text>
           </View>
 
