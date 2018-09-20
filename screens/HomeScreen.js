cbd3695659961ffc86e00de442a38bff41117b7f
Kushagra Trivedi <kusht07@gmail.com>
2018-09-20 01:29:01 -0500

    g

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -44,7 +44,7 @@ export default class HomeScreen extends React.Component {
             <Text style={styles.getStartedText}>
 
 
-              Making earth a better place together. Socialize to help community.
+              Making earth a better place together. Socialize to help community. Test Reload
 
 
             </Text>
